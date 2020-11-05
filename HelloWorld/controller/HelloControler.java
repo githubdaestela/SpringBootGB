c52de1cc5977d764f17e1ab8921744dd0cde220f
gitdaestela <estelacriart@gmail.com>
2020-11-04 21:22:27 -0300

    Hello World!

---
+++ HelloWorld/controller/HelloControler.java
@@ -0,0 +1,15 @@
+package com.helloworld.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/hello")
+public class HelloControler {
+	@GetMapping
+	public String hello() 
+	{
+		return "Hello World!";
+	}
+}
