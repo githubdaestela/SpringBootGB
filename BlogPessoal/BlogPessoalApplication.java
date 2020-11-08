74740f5ccf0a199f1001fdc8f675ff15b17bed8a
gitdaestela <estelacriart@gmail.com>
2020-11-08 16:43:18 -0300

    Blog Pessoal

---
+++ BlogPessoal/BlogPessoalApplication.java
@@ -0,0 +1,14 @@
+package org.generation.blogPessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPessoalApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(BlogPessoalApplication.class, args);
+	}
+}
